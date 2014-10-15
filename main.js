fc887f7adc21231ffdd296aaf9aa800dc7c7959a
Julian Sparber <julian@sparber.net>
2014-10-15 16:39:55 +0200

    [feat] interval inceremts

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-var score = 0;
-var time = 30;
+//var a = new Array();
+//for (var x = 0; x <=60; x++) {a[x] = Math.round (1000 - ([1- Math.pow(2.7, (-x/15))] * 800))}
+
+var score;
+var time;
+var tileInterval;
+var timerIndex = 0;
+var intervalTime= [1000,949,901,856,814,775,738,703,671,641,613,586,561,538,517,496,477,460,443,427,413,399,386,374,363,353,343,334,325,317,310,303,296,290,284,279,274,269,265,260,257,253,250,246,243,241,238,236,233,231,229,227,226,224,222,221,220,218,217,216,215];
+
+calcLUT();
+start();
 alert("This game is under heavy development! Therefore, there are still some bugs.");
 $(".js-time").text("Time: " + time);
 addTile(randomTile(), "blueTile");
@@ -13,9 +22,24 @@ window.setInterval(function(){
     time--;
     $(".js-time").text("Time: " + time);
   }
+
+  timerIndex++;
+  if (intervalTime[timerIndex] === undefined)
+    timerIndex = intervalTime.length - 1;
+  clearInterval(tileInterval);
+  setTileInterval();
+  console.log(intervalTime[timerIndex]);
 }, 1000);
 
-window.setInterval(function(){
+
+function calcLUT() {
+var a = new Array();
+for (var x = 0; x <=60; x++) {a[x] = Math.round (1000 - ([1- Math.pow(2.7, (-x/5))] * 700))}
+  intervalTime = a;
+}
+
+function setTileInterval() {
+tileInterval = window.setInterval(function(){
   var rand = Math.floor(Math.random()*50);
   if (rand === 13 || rand === 29 || rand === 26) {
     addTile(randomTile(), "greenTile");
@@ -23,10 +47,13 @@ window.setInterval(function(){
   else if (rand < 7){
     addTile(randomTile(), "redTile");
   }
-  else
+  else{
     addTile(randomTile(), "blueTile");
+  }
 
-}, 1000);
+}, intervalTime[timerIndex]);
+
+}
 
 
 function gameOver(e) {
@@ -59,7 +86,8 @@ function addTile(index, el) {
 }
 function start() {
   score = 0;
-  time = 30;
+  time = 60;
+  timerIndex = 0;
   $(".js-score").text("Score: " + score);
   $(".js-time").text("Time: " + time);
   removeTile(".grid_tile");
@@ -68,9 +96,7 @@ function start() {
 function select(el) {
   if ($(el).hasClass("blueTile")) {
     score++;
-    addTile(randomTile(), "blueTile");
-    if (isGridFull() === false)
-      removeTile(el);
+    removeTile(el);
   }
   else if ($(el).hasClass("redTile")) {
     removeTile(el);
@@ -89,9 +115,12 @@ function select(el) {
 }
 
 function isGridFull() {
-  if ($(".grid_tile:not(.blueTile)").length <= 1) {
-    return true;
-  }
+  if ($(".grid_tile:not(.blueTile)").length <= 1)
+        return true;
+    if ($(".grid_tile:not(.greenTile)").length <= 1)
+        return true;
+      if ($(".grid_tile:not(.blueTile)").length <= 1)
+        return true;
   return false;
 }
 
