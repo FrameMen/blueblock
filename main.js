8e9ac488577e86aa75301f0862d09d223c06558a
mtamagno <thegreatherochrono@hotmail.it>
2014-10-30 23:01:40 +0100

    [fix] marquee speed bug

--- main.js
+++ main.js
@@ -598,29 +598,34 @@ function setViolence(next){
 (function($) {
 
     var methods = {
+
         init: function(options) {
             this.children(':first').stop();
             this.marquee('play');
         },
+
         play: function() {
             var marquee = this,
                 pixelsPerSecond = 100,
                 firstChild = this.children(':first'),
-                totalHeight = 0,
-                difference,
-                duration;
+                totalHeight = 700,
+                difference = 0,
+                duration = 0;
 
             // Find the total height of the children by adding each child's height:
             this.children().each(function(index, element) {
                 totalHeight += $(element).innerHeight();
             });
+            console.log(totalHeight);
 
             // The distance the divs have to travel to reach -1 * totalHeight:
             difference = totalHeight + parseInt(firstChild.css('margin-top'), 10  );
+            console.log(difference);
 
             // The duration of the animation needed to get the correct speed:
-            duration = 8900;
-            //console.log(duration);
+            duration = (difference/pixelsPerSecond) * 1000;
+            console.log(duration);
+
             // Animate the first child's margin-top to -1 * totalHeight:
             firstChild.animate(
                 { 'margin-top': -1 * totalHeight },
